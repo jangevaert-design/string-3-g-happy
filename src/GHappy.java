b677fa7c160057ae9b84eba48f7323093ee213e2
Jan Gevaert <jan.gevaert@gmail.com>
2020-12-18 11:30:59 -0700

    Created instance.

--- src/GHappy.java
+++ src/GHappy.java
@@ -7,12 +7,13 @@ Return true if all the g's in the given string are happy.
  */
 
   public static void main(String[] args) {
+    GHappy newgHappy = new GHappy();
     String str = "xxggyyggxxg";
 
-    System.out.printf("\nthe string %s gives: " + gHappy(str)+ "\n", str);
+    System.out.printf("\nthe string %s gives: " + newgHappy.gHappy(str)+ "\n", str);
   }
 
-  public static boolean gHappy(String str) {
+  public boolean gHappy(String str) {
     boolean result = true;
 
     for (int i = 0; i < str.length(); i++) {
